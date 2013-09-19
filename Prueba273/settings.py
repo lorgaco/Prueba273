7c04565cf0a6943d7d30f1f55913ee1d7ebe982b
lorgaco <lorenzo.gaco@gmail.com>
2013-09-19 19:22:31 +0200

    prueba_postgres

--- Prueba273/settings.py
+++ Prueba273/settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'Prueba3',
@@ -154,3 +154,19 @@ LOGGING = {
         },
     }
 }
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='postgres://pruebauser:pruebauser@localhost/pruebadb')}
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+ os.path.join(BASE_DIR, 'static'),
+)
